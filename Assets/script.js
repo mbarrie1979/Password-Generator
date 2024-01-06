5b77c7fb871d74324c5cd2bc6cd8515e5d279868
Mark Barrie <mark.a.barrie@gmail.com>
2024-01-06 10:43:57 -0500

    created object with key value pairs representing relevant variables in the password generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,16 @@
 // Assignment Code
+console.log("Check Check 1 2 1 2")
 var generateBtn = document.querySelector("#generate");
+
+// initialization of variables for password 
+
+var passcodeRequirements = {
+  passwordLength: 8,
+  includeUpperCase: false,
+  includeLowerCase: false,
+  includeNumeric: false,
+  includeSpecial: false
+}
 
 // Write password to the #password input
 function writePassword() {
