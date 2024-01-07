44d9cbe485311198f52f6752763b170fe44eb9f2
Mark Barrie <mark.a.barrie@gmail.com>
2024-01-07 12:07:47 -0500

    added code block for password length requirements

--- Assets/script.js
+++ Assets/script.js
@@ -3,16 +3,46 @@ console.log("Check Check 1 2 1 2")
 var generateBtn = document.querySelector("#generate");
 
 // initialization of variables for password 
+var regexpCheckIfNumbers = /^\d+$/;
+
+var lengthCheck = false;
 
 var passcodeRequirements = {
   passwordLength: 8,
-  includeUpperCase: false,
-  includeLowerCase: false,
-  includeNumeric: false,
+  includeLetters: false,
+  includeNumbers: false,
   includeSpecial: false
 }
 
 // Write password to the #password input
+while (!lengthCheck) {
+  var userPromptLength = prompt("Enter the desired password length (from 8 - 12 characters)");
+
+  if (!userPromptLength) {
+    alert("User cancelled");
+    passcodeRequirements.passwordLength = 8;
+  } else if (!userPromptLength.match(regexpCheckIfNumbers)) {
+    alert("You must enter a numeric value from 8 - 12");
+  } else {
+    var parsedLength = parseInt(userPromptLength);
+    if (parsedLength < 8 || parsedLength > 12) {
+      alert("You must enter a value from 8 - 12");
+    } else {
+      passcodeRequirements.passwordLength = parsedLength;
+      console.log(passcodeRequirements)
+      lengthCheck = true;
+
+    }
+  }
+};
+
+
+
+
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
