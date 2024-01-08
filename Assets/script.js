d9343349008a52030297a1a87b2a1964a38bcbd4
Mark Barrie <mark.a.barrie@gmail.com>
2024-01-07 23:30:07 -0500

    added value in object and logic to update it

--- Assets/script.js
+++ Assets/script.js
@@ -9,51 +9,27 @@ var lengthCheck = false;
 var lettersCheck = false;
 var numbersCheck = false;
 var specialCheck = false;
+// var numberOfArrays = 0;
 
 var passcodeRequirements = {
   passwordLength: 8,
   includeLetters: false,
   includeNumbers: false,
-  includeSpecial: false
+  includeSpecial: false,
+  numberOfArrays: 0
 }
 
 
-
-
-var lowerCaseLetters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
-var upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
-var numberArray = [...Array(10)].map((_, i) => i);
-var specialCharacters = [...Array(26)].map((_, i) => String.fromCharCode(i + 33));
-
-
-var shuffle = (input) => {
-  for (var i = input.length - 1; i > 0; i--); {
-    var j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j] = array[j], array[i]];
-  }
-  return input;
-}
-
-var randomChoose = (arr) => {
-  var h = Math.floor(Math.random() * arr.length);
-  return arr[h]; // Return the element at the randomly chosen index
-};
-
-var runRandomChooseMultipleTimes = (arr, times) => {
-  var results = [];
-  for (var i = 0; i < times; i++) {
-    results.push(randomChoose(arr));
-  }
-  return results;
-};
 
 // User Enters in Desired length of password 8-12 characters
 while (!lengthCheck) {
   var userPromptLength = prompt("Enter the desired password length (from 8 - 12 characters)");
 
   if (!userPromptLength) {
-    alert("User cancelled");
+    alert("User cancelled, will use default value of 8");
     passcodeRequirements.passwordLength = 8;
+    console.log(passcodeRequirements)
+    break;
   } else if (!userPromptLength.match(regexpCheckIfNumbers)) {
     alert("You must enter a numeric value from 8 - 12");
   } else {
@@ -75,12 +51,14 @@ while (!lettersCheck) {
   if (!userLetters) {
     alert("User cancelled, default of YES will be used");
     passcodeRequirements.includeLetters = true;
+    passcodeRequirements.numberOfArrays += 1;
     console.log(passcodeRequirements)
     lettersCheck = true;
     break;
   } else {
     if (userLetters.trim()[0].toUpperCase() === "Y") {
       passcodeRequirements.includeLetters = true;
+      passcodeRequirements.numberOfArrays += 1;
       console.log(passcodeRequirements)
       lettersCheck = true;
     } else if (userLetters.trim()[0].toUpperCase() === "N") {
@@ -100,13 +78,15 @@ while (!numbersCheck) {
   if (!userNumbers) {
     alert("User cancelled, default of YES will be used");
     passcodeRequirements.includeNumbers = true;
+    passcodeRequirements.numberOfArrays += 1;
     console.log(passcodeRequirements)
     numbersCheck = true;
     break;
   } else {
     if (userNumbers.trim()[0].toUpperCase() === "Y") {
       passcodeRequirements.includeNumbers = true;
-      console.log(passcodeRequirements)
+      passcodeRequirements.numberOfArrays += 1;
+      console.log(passcodeRequirements);
       numbersCheck = true;
     } else if (userNumbers.trim()[0].toUpperCase() === "N") {
       passcodeRequirements.includeNumbers = false;
@@ -125,12 +105,14 @@ while (!specialCheck) {
   if (!userSpecial) {
     alert("User cancelled, default of YES will be used");
     passcodeRequirements.includeSpecial = true;
-    console.log(passcodeRequirements)
+    passcodeRequirements.numberOfArrays += 1;
+    console.log(passcodeRequirements);
     specialCheck = true;
     break;
   } else {
     if (userSpecial.trim()[0].toUpperCase() === "Y") {
       passcodeRequirements.includeSpecial = true;
+      passcodeRequirements.numberOfArrays += 1;
       console.log(passcodeRequirements)
       specialCheck = true;
     } else if (userSpecial.trim()[0].toUpperCase() === "N") {
@@ -145,7 +127,33 @@ while (!specialCheck) {
 }
 
 
+var lowerCaseLetters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+var upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
+var allLetters = [lowerCaseLetters, upperCaseLetters];
+var numberArray = [...Array(10)].map((_, i) => i);
+var specialCharacters = [...Array(26)].map((_, i) => String.fromCharCode(i + 33));
 
+
+var shuffle = (input) => {
+  for (var i = input.length - 1; i > 0; i--); {
+    var j = Math.floor(Math.random() * (i + 1));
+    [input[i], input[j] = input[j], input[i]];
+  }
+  return input;
+}
+
+var randomChoose = (arr) => {
+  var h = Math.floor(Math.random() * arr.length);
+  return arr[h]; // Return the element at the randomly chosen index
+};
+
+var runRandomChooseMultipleTimes = (arr, times) => {
+  var results = [];
+  for (var i = 0; i < times; i++) {
+    results.push(randomChoose(arr));
+  }
+  return results;
+};
 
 
 
